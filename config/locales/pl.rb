08592f2e30a0b594a713d08c8cbaff950976e728
Mariusz Wyrozębski <mariuszwyrozebski@gmail.com>
2012-04-04 19:53:39 +0200

    force encoding?

---
+++ config/locales/pl.rb
@@ -0,0 +1,30 @@
+{
+    :pl => {
+        :current_locale_en => "English",
+        :current_locale_pl => "Polski",
+        :portfolio => "Portfolio",
+        :services => "Usługi",
+        :menu => {
+            :contact => "Kontakt"
+        },
+        :contact => {
+            :phones => "Phones",
+            :office => "Biuro",
+            :mobile => "Komórka",
+            :address => "Adres",
+            :email => "mariuszwyrozebski@gmail.com"
+        },
+        :choirhouse => {
+            :header => "Choirhouse - platforma dla chórzystów",
+            :description => "Współpraca i wspólne kontakty to podstawa życia chóralnego. Aplikacja umożliwia wzajemne kontakty i ćwiczenia nad materiałem."
+        },
+        :select6 => {
+            :header => "Select6 - społeczne finansowanie projektów",
+            :description => "Select6 pomage znaleźć potrzebne fundusze na rozpoczęcie własnego innowacyjnego projektu z pomocą sieci społecznościowych. Wyróżnia się ukierunkowaniem na mniejszości etniczne i wsparciem dla projektów spoza U.S."
+        },
+        :cowtrip => {
+            :header => "Cowtrip - serwis poświęcony dojeżdzającym do pracy.",
+            :description => "Cowtrip zwraca szczególną uwagę na koszt dojazdów do pracy i zachęca do szukania pracy bliżej miejsca zamieszkania."
+        }
+    }
+}
