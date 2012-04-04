23532cfea4903eda37ffa247e08a680d44331e72
Mariusz Wyrozębski <mariuszwyrozebski@gmail.com>
2012-04-04 19:44:12 +0200

    force encoding?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :set_locale
+  before_filter :set_charset
 
   def set_locale
     I18n.locale = session[:locale] || extract_locale_from_accept_language_header || I18n.default_locale
@@ -9,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def default_url_options(options={})
     logger.debug "default_url_options is passed options: #{options.inspect}\n"
-    { :locale => I18n.locale }
+    {:locale => I18n.locale}
   end
 
   private
@@ -17,6 +18,10 @@ class ApplicationController < ActionController::Base
     if request.env['HTTP_ACCEPT_LANGUAGE']
       request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
     end
+  end
+
+  def set_charset
+    @headers["Content-Type"] = "text/html; charset=utf-8"
   end
 
 end
