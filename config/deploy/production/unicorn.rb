ca0e8fff8a0c94b4ee91559c1d429fb8cbe249da
Mariusz Wyrozębski <mariuszwyrozebski@gmail.com>
2012-06-26 14:01:46 +0200

    only 2 workers will be enough

--- config/deploy/production/unicorn.rb
+++ config/deploy/production/unicorn.rb
@@ -15,7 +15,7 @@ APP_ROOT = "/home/rails/redwarrior/current"
 ENV['BUNDLE_GEMFILE'] = File.expand_path('../../../Gemfile', File.dirname(__FILE__))
 require 'bundler/setup'
 
-worker_processes 4
+worker_processes 2
 working_directory APP_ROOT
 
 preload_app true
