0e1d5eef0fd25c98ef54117e5175cb0c7104f49f
Mariusz Wyrozębski <mariuszwyrozebski@gmail.com>
2012-06-26 13:49:36 +0200

    fix css? and deploy to new server

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,25 +3,43 @@ set :repository, "git@github.com:m4risU/redwarrior.git"
 set :scm, :git
 set :scm_user, "m4risu"
 
-set :use_sudo, true
-set :deploy_to, "/srv/redwarrior.pl/"
+set :use_sudo, false
+#set :deploy_to, "/srv/redwarrior.pl/"
+set :deploy_to, "/home/rails/redwarrior/"
 
 set :keep_releases, 5
 after "deploy:update", "deploy:cleanup"
 
+#server "5.9.31.163", :app, :web, :db, :primary => true
 server "redwarrior.pl", :app, :web, :db, :primary => true
+
 set :rails_env, "production"
 set :user, "rails"
 
+set :default_environment, {
+    'PATH' => "/home/rails/.rvm/gems/ruby-1.9.3-p125@redwarrior/bin:/home/rails/.rvm/gems/ruby-1.9.3-p125@global/bin:/home/rails/.rvm/rubies/ruby-1.9.3-p125/bin:/home/rails/.rvm/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games",
+    'GEM_HOME' => '/home/rails/.rvm/gems/ruby-1.9.3-p125@redwarrior',
+    'GEM_PATH' => '/home/rails/.rvm/gems/ruby-1.9.3-p125@redwarrior:/home/rails/.rvm/gems/ruby-1.9.3-p125@global',
+    'BUNDLE_PATH' => ''
+}
+
 after "deploy:update", "deploy:copy_yml_files"
+after "deploy:update", "deploy:rvm_init"
+after "deploy:rvm_init", "deploy:bundle"
+before "deploy:restart", "deploy:bundle"
+
+deploy.task "rvm_init" do
+  run "cd #{release_path}; echo 'rvm use 1.9.3-p125@redwarrior' > #{release_path}/.rvmrc"
+  #run "cd #{release_path}; gem env"
+end
 
 deploy.task :copy_yml_files do
-  run "ln -s /srv/redwarrior.pl/etc/config/database.yml /srv/redwarrior.pl/current/config/database.yml"
+  run "ln -s /home/rails/redwarrior/etc/config/database.yml /home/rails/redwarrior/current/config/database.yml"
 end
 
 deploy.task :bundle do
   #run "cd #{release_path}; sudo bundle install --deployment" if there are git repos!
-  run "cd #{release_path}; sudo bundle install"
+  run "cd #{release_path}; bundle install --path /home/rails/.rvm/gems/ruby-1.9.3-p125@redwarrior"
 end
 
 deploy.task :restart, :roles => :app do
@@ -33,7 +51,6 @@ deploy.task :restart, :roles => :app do
   # separate thin service with wrapper!
   # http://stackoverflow.com/questions/3230404/rvm-and-thin-root-vs-local-user
   #run "sudo service thin_s6 restart"
-  run "cd #{release_path}; sudo bundle install"
-  run "cd #{release_path}; RAILS_ENV=#{rails_env} sudo bundle exec rake assets:precompile"
-  run "sudo service thin_rw restart"
+  run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec rake assets:precompile"
+  run "sudo service redwarrior restart"
 end
